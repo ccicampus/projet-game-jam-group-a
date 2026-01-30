bc19de9cb04b5beadcc94c2ab68948c4030689b0
Alex MALVY <142165167+AlexMalvy@users.noreply.github.com>
2026-01-30 23:19:52 +0100

    chore(minigame): add timer and win/lose condition

--- Assets/_Project/Scripts/Minigame/Minigame.cs
+++ Assets/_Project/Scripts/Minigame/Minigame.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class Minigame : MonoBehaviour
 {
     public int totalHits;
+    public float timeout;
     public Text textObject;
     private float horizontalInput;
     private bool right = true;
@@ -16,23 +17,35 @@ public class Minigame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = InputManager.Instance.MoveInput.x;
+        timeout -= Time.deltaTime;
+        if (timeout < 0 && totalHits > 0)
+        {
+            Debug.Log("dead");
+            textObject.text = "dead";
+        } else
+        {
+            if (totalHits <= 0)
+            {
+                Debug.Log("win");
+                textObject.text = "win";
+            } else
+            {
+                horizontalInput = InputManager.Instance.MoveInput.x;
 
-        if (right && horizontalInput == 1)
-        {
-            right = !right;
-            totalHits -= 1;
-            Debug.Log(totalHits);
-            textObject.text = totalHits.ToString();
-        } else if (!right && horizontalInput == -1)
-        {
-            right = !right;
-            totalHits -= 1;
-            Debug.Log(totalHits);
-            textObject.text = totalHits.ToString();
+                if (right && horizontalInput == 1)
+                {
+                    right = !right;
+                    totalHits -= 1;
+                    Debug.Log(totalHits);
+                    textObject.text = totalHits.ToString();
+                } else if (!right && horizontalInput == -1)
+                {
+                    right = !right;
+                    totalHits -= 1;
+                    Debug.Log(totalHits);
+                    textObject.text = totalHits.ToString();
+                }
+            }
         }
-
-
-        
     }
 }
