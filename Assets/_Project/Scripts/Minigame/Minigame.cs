768d700055b87a1cb3b9dc108980eb7910be3d9c
Alex MALVY <142165167+AlexMalvy@users.noreply.github.com>
2026-01-31 02:17:35 +0100

    feat(minigame): add tutorial phase before minigame

--- Assets/_Project/Scripts/Minigame/Minigame.cs
+++ Assets/_Project/Scripts/Minigame/Minigame.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,47 +6,81 @@ public class Minigame : MonoBehaviour
 {
     public int totalHits;
     public float timeout;
-    public Text textObject;
+    public TextMeshProUGUI hpText;
+    public TextMeshProUGUI timerText;
+    public Sprite imageState0;
+    public Sprite imageState1;
+    public Sprite tutorialImage;
+    public Image background;
     private float horizontalInput;
     private bool right = true;
+    private bool tutorial = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        textObject.text = totalHits.ToString();
+        background.sprite = tutorialImage;
+        hpText.text = "";
+        timerText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeout -= Time.deltaTime;
-        if (timeout < 0 && totalHits > 0)
+        if (tutorial)
         {
-            Debug.Log("dead");
-            textObject.text = "dead";
+            if (InputManager.Instance.JumpPressed)
+            {
+                tutorial = false;
+                hpText.text = totalHits.ToString();
+            }
         } else
         {
-            if (totalHits <= 0)
+            timeout -= Time.deltaTime;
+            timerText.text = $"{timeout:F2} s";
+            updateSprite();
+
+            if (timeout < 0 && totalHits > 0)
             {
-                Debug.Log("win");
-                textObject.text = "win";
+                hpText.text = "dead";
             } else
             {
-                horizontalInput = InputManager.Instance.MoveInput.x;
-
-                if (right && horizontalInput == 1)
+                if (totalHits <= 0)
                 {
-                    right = !right;
-                    totalHits -= 1;
-                    Debug.Log(totalHits);
-                    textObject.text = totalHits.ToString();
-                } else if (!right && horizontalInput == -1)
+                    hpText.text = "win";
+                } else
                 {
-                    right = !right;
-                    totalHits -= 1;
-                    Debug.Log(totalHits);
-                    textObject.text = totalHits.ToString();
+                    processHit();
                 }
             }
+        }
+    }
+
+    void updateSprite()
+    {
+        if (right)
+        {
+            background.sprite = imageState0;
+        } else
+        {
+            background.sprite = imageState1;
+        }
+    }
+
+    void processHit()
+    {
+        horizontalInput = InputManager.Instance.MoveInput.x;
+
+        if (right && horizontalInput == 1)
+        {
+            right = !right;
+            totalHits -= 1;
+            hpText.text = totalHits.ToString();
+        } else if (!right && horizontalInput == -1)
+        {
+            right = !right;
+            totalHits -= 1;
+            hpText.text = totalHits.ToString();
         }
     }
 }
